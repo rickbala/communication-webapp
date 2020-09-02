01e1af056d9cca8a9608ab44c04129a7d9de7bbc
rickbala <ricardovbalarini@gmail.com>
2020-09-01 21:05:18 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var p = 0;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  //number of peeps plus
+  io.emit('updateP', ++p);
+
+  //number of peeps down
+  socket.on('disconnect', () => {
+    io.emit('updateP', --p);
+  });
+
+  //chat message
+  socket.on('chat message', (msg, box) => {
+    //emits a signal that will update the rigth box with a new message
+    io.emit('chat message', msg, box);
+  });
+
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
